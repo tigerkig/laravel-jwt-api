98c1be50c297e6346547ee7fe8f3a0a3cf4318e3
tigerkig <tigerkig714@outlook.com>
2023-03-31 22:04:26 -0700

    update code for new insight

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\InsightsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +16,17 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
+Route::group([ 'middleware' => 'api', 'prefix' => 'auth' ], function ($router) {
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
+    Route::get('/user-profile', [AuthController::class, 'userProfile'])->name('auth.userProfile'); 
+});
 
-], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+Route::group([ 'middleware' => 'api', 'prefix' => 'news' ], function () {
+    Route::get('/', [InsightsController::class, 'all'])->name('news.all');
+    Route::get('/{id}', [InsightsController::class, 'detail'])->name('news.detail');
+    Route::post('/add', [InsightsController::class, 'store'])->name('news.store');
+
 });
