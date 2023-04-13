7d42d9465f95e0c906518ec01c78a0a6c4552829
tigerkig <tigertopdev714@gmail.com>
2023-04-13 20:10:38 +0900

    update get request methods

--- routes/api.php
+++ routes/api.php
@@ -41,12 +41,17 @@ Route::group([ 'middleware' => 'api', 'prefix' => 'news' ], function () {
     Route::delete('/{id}', [InsightsController::class, 'delete'])->name('news.delete');
 });
 
+Route::get('/api/news', [InsightsController::class, 'all'])->name('news.all');
+Route::get('/api/news/{id}', [InsightsController::class, 'detail'])->name('news.detail');
 
 Route::group([ 'middleware' => 'api', 'prefix' => 'teams' ], function () {
     Route::post('/update/{id}', [TeamMemberController::class, 'update'])->name('teams.update');
     Route::post('/add', [TeamMemberController::class, 'store'])->name('teams.store');
     Route::delete('/{id}', [TeamMemberController::class, 'delete'])->name('teams.delete');
 });
+
+Route::get('/api/teams', [TeamMemberController::class, 'all'])->name('teams.all');
+Route::get('/api/teams/{id}', [TeamMemberController::class, 'detail'])->name('teams.detail');
 
 Route::group([ 'middleware' => 'api', 'prefix' => 'volunteer' ], function () {
     // Volunteer Request apis
@@ -60,11 +65,19 @@ Route::group([ 'middleware' => 'api', 'prefix' => 'volunteer' ], function () {
     Route::delete('/description/{id}', [VolunteerController::class, 'deleteDescription'])->name('volunteer_description.delete');
 });
 
+Route::get('/api/volunteer/request', [VolunteerController::class, 'all'])->name('volunteer_request.all');
+Route::get('/api/volunteer/request/{id}', [VolunteerController::class, 'detail'])->name('volunteer_request.detail');
+Route::get('/api/volunteer/description', [VolunteerController::class, 'allDescription'])->name('volunteer_description.all');
+Route::get('/api/volunteer/description/{id}', [VolunteerController::class, 'detailDescription'])->name('volunteer_description.detail');
+
 Route::group([ 'middleware' => 'api', 'prefix' => 'organization' ], function () {
     Route::post('/add', [OrganizationController::class, 'store'])->name('organization.store');
     Route::post('/update/{id}', [OrganizationController::class, 'update'])->name('organization.update');
     Route::delete('/{id}', [OrganizationController::class, 'destroy'])->name('organization.destroy');
 });
+
+Route::get('/api/organization', [OrganizationController::class, 'index'])->name('organization.index');
+Route::get('/api/organization/{id}', [OrganizationController::class, 'show'])->name('organization.show');
 
 Route::group([ 'middleware' => 'api', 'prefix' => 'fundraiser' ], function () {
     Route::post('/add', [FundraiserController::class, 'store'])->name('fundraiser.store');
@@ -77,7 +90,13 @@ Route::group([ 'middleware' => 'api', 'prefix' => 'fundraiser' ], function () {
     Route::patch('/fundraiser', [FundraiserCommentsController::class, 'fundraiser'])->name('fundraiserComment.fundraiser');
 });
 
+Route::get('/api/fundraiser', [FundraiserController::class, 'index'])->name('fundraiser.index');
+Route::get('/api/fundraiser/{id}', [FundraiserController::class, 'show'])->name('fundraiser.show');
+Route::get('/api/fundraiser/{fundraiser_id}/supporters', [SupporterController::class, 'index'])->name('supporter.index');
+Route::get('/api/fundraiser/{id}/comments', [FundraiserCommentsController::class, 'index'])->name('fundraiserComment.index');
+
 Route::group([ 'middleware' => 'api', 'prefix' => 'supporter' ], function () {
+    Route::get('/{id}', [SupporterController::class, 'show'])->name('supporter.show');
     Route::post('/add', [SupporterController::class, 'store'])->name('supporter.store');
     Route::post('/update/{id}', [SupporterController::class, 'update'])->name('supporter.update');
     Route::delete('/{id}', [SupporterController::class, 'destroy'])->name('supporter.destroy');
@@ -89,9 +108,14 @@ Route::group([ 'middleware' => 'api', 'prefix' => 'faq' ], function () {
     Route::delete('/{id}', [FaqController::class, 'delete'])->name('faq.delete');
 });
 
+Route::get('/api/faq', [FaqController::class, 'all'])->name('faq.all');
+Route::get('/api/faq/{id}', [FaqController::class, 'detail'])->name('faq.detail');
+
 Route::group([ 'middleware' => 'api', 'prefix' => 'contact' ], function () {
     Route::post('/add', [ContactController::class, 'store'])->name('contact.store');
 });
+
+Route::get('/api/contact', [ContactController::class, 'all'])->name('contact.all');
 
 Route::group([ 'middleware' => 'api', 'prefix' => 'carousel' ], function () {
     Route::post('/add', [CarouselController::class, 'store'])->name('carousel.store');
@@ -99,35 +123,18 @@ Route::group([ 'middleware' => 'api', 'prefix' => 'carousel' ], function () {
     Route::delete('/{id}', [CarouselController::class, 'delete'])->name('carousel.delete');
 });
 
+Route::get('/api/carousel', [CarouselController::class, 'all'])->name('carousel.all');
+
 Route::group([ 'middleware' => 'api', 'prefix' => 'review' ], function () {
     Route::post('/add', [ReviewController::class, 'store'])->name('review.store');
     Route::post('/{id}', [ReviewController::class, 'update'])->name('review.update');
     Route::delete('/{id}', [ReviewController::class, 'delete'])->name('review.delete');
 });
 
+Route::get('/api/review', [ReviewController::class, 'all'])->name('review.all');
+Route::get('/api/review/{id}', [ReviewController::class, 'show'])->name('review.show');
+
 Route::group([ 'middleware' => 'api', 'prefix' => 'payment' ], function () {
     Route::get('/success/{payment_id}', [PaymentController::class, 'successTransaction'])->name('payment.success');
     Route::get('/cancel/{payment_id}', [PaymentController::class, 'cancelTransaction'])->name('payment.cancelled');
 });
-
-Route::get('/', [TeamMemberController::class, 'all'])->name('teams.all');
-Route::get('/{id}', [TeamMemberController::class, 'detail'])->name('teams.detail');
-Route::get('/', [InsightsController::class, 'all'])->name('news.all');
-Route::get('/{id}', [InsightsController::class, 'detail'])->name('news.detail');
-Route::get('/request', [VolunteerController::class, 'all'])->name('volunteer_request.all');
-Route::get('/request/{id}', [VolunteerController::class, 'detail'])->name('volunteer_request.detail');
-Route::get('/description', [VolunteerController::class, 'allDescription'])->name('volunteer_description.all');
-Route::get('/description/{id}', [VolunteerController::class, 'detailDescription'])->name('volunteer_description.detail');
-Route::get('/', [OrganizationController::class, 'index'])->name('organization.index');
-Route::get('/{id}', [OrganizationController::class, 'show'])->name('organization.show');
-Route::get('/', [FundraiserController::class, 'index'])->name('fundraiser.index');
-Route::get('/{id}', [FundraiserController::class, 'show'])->name('fundraiser.show');
-Route::get('/{fundraiser_id}/supporters', [SupporterController::class, 'index'])->name('supporter.index');
-Route::get('/{id}/comments', [FundraiserCommentsController::class, 'index'])->name('fundraiserComment.index');
-Route::get('/{id}', [SupporterController::class, 'show'])->name('supporter.show');
-Route::get('/', [ContactController::class, 'all'])->name('contact.all');
-Route::get('/', [FaqController::class, 'all'])->name('faq.all');
-Route::get('/{id}', [FaqController::class, 'detail'])->name('faq.detail');
-Route::get('/', [CarouselController::class, 'all'])->name('carousel.all');
-Route::get('/', [ReviewController::class, 'all'])->name('review.all');
-Route::get('/{id}', [ReviewController::class, 'show'])->name('review.show');
